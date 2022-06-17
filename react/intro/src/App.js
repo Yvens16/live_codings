7186443a917f9afab67b8635906c3d2b633d1e7c
Yvens16 <yvensbelaston@gmail.com>
2022-06-17 10:49:27 +0200

    useEffect course

--- react/intro/src/App.js
+++ react/intro/src/App.js
@@ -2,13 +2,16 @@ import Intro from './useEffect/Intro'
 import AppelApi from './useEffect/AppelApi'
 import CustomUpdate from './useEffect/CustomUpdate'
 // TODO: LIRE LE LIEN DE LA DOC REACT:  https://reactjs.org/docs/hooks-effect.html
+/**
+ * Décommenter le composant qui vous intéresse avant d'aller sur le composant en question
+*/
 
 
 function App () {  
   return <>
   {/* <Intro/> */}
   {/* <AppelApi/> */}
-  <CustomUpdate/>
+  {/* <CustomUpdate/> */}
   </>
 }
 
