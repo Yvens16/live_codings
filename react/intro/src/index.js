76a6327d910a23ae666e0b67dd1b7868af8d7cde
Yvens16 <yvensbelaston@gmail.com>
2022-06-30 09:34:23 +0200

    useContext avec plusieurs contextes

--- react/intro/src/index.js
+++ react/intro/src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+import './index.css'
+// import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// 1
+import React, { createContext, useContext } from 'react'
+const NameContext = createContext(null)
+const AgeContext = createContext(null)
+
+// 1- Créer le context
+// 2- Remplir le context avec mes données
+// 3- Utiliser les données du context
+
+function App() {
+  // 3-
+  const {name} = useContext(NameContext);
+  const {age} = useContext(AgeContext);
+  return <div>Hello, {name} a {age} ans</div>;
+}
+
+function Info() {
+  const {name} = useContext(NameContext);
+  return <div>Infos de {name}</div>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <AgeContext.Provider value={{age: 26}}>
+    <NameContext.Provider value={{ name: 'Yvens' }}>
+      <Info/>
+      <App />
+    </NameContext.Provider>
+    </AgeContext.Provider>
+    {/* 2- */}
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
